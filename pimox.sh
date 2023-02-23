c9689e5afaea11df4e844886a76e2a9bca9ce647
Lotustrailtheprestigefig <102186419+Lotustrailtheprestigefig@users.noreply.github.com>
2023-02-23 21:47:23 +0100

    Update pimox.sh

--- pimox.sh
+++ pimox.sh
@@ -2,6 +2,7 @@
 
 # raspbian arm x64 needed
 sudo -s
+apt update && apt upgrade -y
 curl https://raw.githubusercontent.com/pimox/pimox7/master/RPiOS64-IA-Install.sh > RPiOS64-IA-Install.sh
 chmod +x RPiOS64-IA-Install.sh
 ./RPiOS64-IA-Install.sh
